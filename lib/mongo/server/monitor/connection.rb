aabe7bd3488b33470e66fe7fc401c643777ee69f
Jonathan Hyman <jon@braze.com>
2020-02-24 10:17:44 -0500

    Tag some log statements to not show up in Papertrail.

--- lib/mongo/server/monitor/connection.rb
+++ lib/mongo/server/monitor/connection.rb
@@ -217,7 +217,7 @@ module Mongo
             log_warn("Asked to handshake with #{address} but there was no app metadata provided")
           end
         rescue => e
-          log_warn("Failed to handshake with #{address}: #{e.class}: #{e}:\n#{e.backtrace[0..5].join("\n")}")
+          log_warn("[jontest] Failed to handshake with #{address}: #{e.class}: #{e}:\n#{e.backtrace[0..5].join("\n")}")
           raise
         end
 
